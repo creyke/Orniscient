9fb0f368cfb0033e9a5699bdc00165eced7dab9b
akosradler <akos.radler@gmail.com>
2017-12-10 20:08:22 +0000

    Cleaning up the solution

--- Derivco.Orniscient/Derivco.Orniscient.Proxy.Tests/Grains/TestFixtures/DashboardCollectorGrainTestFixture.cs
+++
@@ -1,5 +0,0 @@
-namespace Derivco.Orniscient.Proxy.Tests.Grains.TestFixtures
-{
-    public class DashboardCollectorGrainTestFixture : BaseTestFixture
-    {}
-}
--- Derivco.Orniscient/Orniscient.Proxy.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Orniscient.Proxy.Models
-{
-    public class Class1
-    {
-    }
-}
